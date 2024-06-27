4465d255fb505b7aad578b959095be21f4857df9
Sven Bledt <sven.bledt@gmail.com>
2024-06-27 14:35:00 +0200

    Update Intents

--- src/client/DiscordBot.js
+++ src/client/DiscordBot.js
@@ -6,86 +6,97 @@ const CommandsListener = require("./handler/CommandsListener");
 const ComponentsHandler = require("./handler/ComponentsHandler");
 const ComponentsListener = require("./handler/ComponentsListener");
 const EventsHandler = require("./handler/EventsHandler");
-const { QuickYAML } = require('quick-yaml.db');
+const { QuickYAML } = require("quick-yaml.db");
 
 class DiscordBot extends Client {
-    collection = {
-        application_commands: new Collection(),
-        message_commands: new Collection(),
-        message_commands_aliases: new Collection(),
-        components: {
-            buttons: new Collection(),
-            selects: new Collection(),
-            modals: new Collection(),
-            autocomplete: new Collection()
-        }
+  collection = {
+    application_commands: new Collection(),
+    message_commands: new Collection(),
+    message_commands_aliases: new Collection(),
+    components: {
+      buttons: new Collection(),
+      selects: new Collection(),
+      modals: new Collection(),
+      autocomplete: new Collection(),
+    },
+  };
+  rest_application_commands_array = [];
+  login_attempts = 0;
+  login_timestamp = 0;
+  statusMessages = [
+    { name: "Checking character data", type: 4 },
+    { name: "Doing some requests on Warmane", type: 4 },
+  ];
+
+  commands_handler = new CommandsHandler(this);
+  components_handler = new ComponentsHandler(this);
+  events_handler = new EventsHandler(this);
+  database = new QuickYAML(config.database.path);
+
+  constructor() {
+    super({
+      intents: 13827, // Combined bitfield for the necessary intents
+      partials: [
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.Message,
+        Partials.Reaction,
+        Partials.User,
+      ],
+      presence: {
+        activities: [
+          {
+            name: "keep this empty",
+            type: 4,
+            state: "Watching some cool stuff",
+          },
+        ],
+      },
+    });
+
+    new CommandsListener(this);
+    new ComponentsListener(this);
+  }
+
+  startStatusRotation = () => {
+    let index = 0;
+    setInterval(() => {
+      this.user.setPresence({ activities: [this.statusMessages[index]] });
+      index = (index + 1) % this.statusMessages.length;
+    }, 4000);
+  };
+
+  connect = async () => {
+    warn(
+      `Attempting to connect to the Discord bot... (${this.login_attempts + 1})`
+    );
+
+    this.login_timestamp = Date.now();
+
+    try {
+      await this.login(process.env.CLIENT_TOKEN);
+      this.commands_handler.load();
+      this.components_handler.load();
+      this.events_handler.load();
+      this.startStatusRotation();
+
+      warn(
+        "Attempting to register application commands... (this might take a while!)"
+      );
+      await this.commands_handler.registerApplicationCommands(
+        config.development
+      );
+      success(
+        "Successfully registered application commands. For specific guild? " +
+          (config.development.enabled ? "Yes" : "No")
+      );
+    } catch (err) {
+      error("Failed to connect to the Discord bot, retrying...");
+      error(err);
+      this.login_attempts++;
+      setTimeout(this.connect, 5000);
     }
-    rest_application_commands_array = [];
-    login_attempts = 0;
-    login_timestamp = 0;
-    statusMessages = [
-        { name: 'Checking character data', type: 4 },
-        { name: 'Doing some requests on Warmane', type: 4 }
-    ];
-
-    commands_handler = new CommandsHandler(this);
-    components_handler = new ComponentsHandler(this);
-    events_handler = new EventsHandler(this);
-    database = new QuickYAML(config.database.path);
-
-    constructor() {
-        super({
-            intents: 3276799,
-            partials: [
-                Partials.Channel,
-                Partials.GuildMember,
-                Partials.Message,
-                Partials.Reaction,
-                Partials.User,
-            ],
-            presence: {
-                activities: [{
-                    name: 'keep this empty',
-                    type: 4,
-                    state: 'Watching some cool stuff',
-                }]
-            }
-        });
-        
-        new CommandsListener(this);
-        new ComponentsListener(this);
-    }
-
-    startStatusRotation = () => {
-        let index = 0;
-        setInterval(() => {
-            this.user.setPresence({ activities: [this.statusMessages[index]] });
-            index = (index + 1) % this.statusMessages.length;
-        }, 4000);
-    }
-
-    connect = async () => {
-        warn(`Attempting to connect to the Discord bot... (${this.login_attempts + 1})`);
-
-        this.login_timestamp = Date.now();
-
-        try {
-            await this.login(process.env.CLIENT_TOKEN);
-            this.commands_handler.load();
-            this.components_handler.load();
-            this.events_handler.load();
-            this.startStatusRotation();
-
-            warn('Attempting to register application commands... (this might take a while!)');
-            await this.commands_handler.registerApplicationCommands(config.development);
-            success('Successfully registered application commands. For specific guild? ' + (config.development.enabled ? 'Yes' : 'No'));
-        } catch (err) {
-            error('Failed to connect to the Discord bot, retrying...');
-            error(err);
-            this.login_attempts++;
-            setTimeout(this.connect, 5000);
-        }
-    }
+  };
 }
 
 module.exports = DiscordBot;
