4e1910d802cd432d9d550513fc8d218e620380a8
Sven Bledt <sven.bledt@gmail.com>
2024-07-02 12:21:19 +0200

    feat: Servertime implemented

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ A Discord bot that handles Warmane's API and provides useful information for pla
 - **Blacklist**: Add or remove a character from the blacklist to prevent him from joining any discord the bot is on.
 - **Character Nickname**: Set a nickname on discord based on the character's name.
 - **Settings**: Change the bot's settings. (disable or enable features)
+- **Servertime**: Get the current servertime of the server.
 
 ## ToDo
 
-- Add report command to report someone for the global blacklist.
-- Global blacklist report response. (Let the developers decide if the report is valid or not)
+- Let ppl adjust the message when they someone joins the discord.
---
+++ src/commands/Information/slashcommand-servertime.js
@@ -0,0 +1,42 @@
+const {
+  EmbedBuilder,
+  ChatInputCommandInteraction,
+  ApplicationCommandOptionType,
+} = require("discord.js");
+const DiscordBot = require("../../client/DiscordBot");
+const ApplicationCommand = require("../../structure/ApplicationCommand");
+
+module.exports = new ApplicationCommand({
+  command: {
+    name: "servertime",
+    description: "Get the servertime of Warmane",
+    type: 1,
+    options: [],
+  },
+  options: {
+    cooldown: 10000,
+    botDevelopers: true,
+  },
+  /**
+   *
+   * @param {DiscordBot} client
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  run: async (client, interaction) => {
+    // print the actual time of Serbia to the interaction channel
+    const date = new Date();
+    const time = date.toLocaleTimeString("en-US", {
+      timeZone: "Europe/Belgrade",
+    });
+    const embed = new EmbedBuilder()
+      .setTitle("Servertime")
+      .setDescription(`The current servertime is: ${time}`)
+      .setColor("RANDOM")
+      .setTimestamp()
+      .setFooter({
+        text: "Requested by: " + interaction.user.tag,
+        iconURL: interaction.user.avatarURL(),
+      });
+    await interaction.reply({ embeds: [embed] });
+  },
+}).toJSON();
