efafd788bf130b3abf50b65bfe8c6b69c92c47ba
Sven Bledt <sven.bledt@gmail.com>
2024-07-05 12:27:07 +0200

    fix: Not showing help and context charname in help

--- src/commands/Information/slashcommand-help.js
+++ src/commands/Information/slashcommand-help.js
@@ -3,25 +3,29 @@ const DiscordBot = require("../../client/DiscordBot");
 const ApplicationCommand = require("../../structure/ApplicationCommand");
 
 module.exports = new ApplicationCommand({
-    command: {
-        name: 'help',
-        description: 'Replies with a list of available application commands.',
-        type: 1,
-        options: [],
-        ephemeral: true
-    },
-    options: {
-        cooldown: 10000
-    },
-    /**
-     * 
-     * @param {DiscordBot} client 
-     * @param {ChatInputCommandInteraction} interaction 
-     */
-    run: async (client, interaction) => {
-        await interaction.reply({
-            content: `${client.collection.application_commands.map((cmd) => '\`/' + cmd.command.name + '\`').join(', ')}`,
-            ephemeral: true
-        });
-    }
+  command: {
+    name: 'help',
+    description: 'Replies with a list of available application commands.',
+    type: 1,
+    options: [],
+    ephemeral: true
+  },
+  options: {
+    cooldown: 10000
+  },
+  /**
+   *
+   * @param {DiscordBot} client
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  run: async (client, interaction) => {
+    // Reply with a list of available application commands. but exclude the help command and only include slashcommands. (no usercontext commands)
+    await interaction.reply({
+      content: `${client.collection.application_commands
+        .filter((cmd) => cmd.command.name !== "help" && cmd.command.name !== "Ask for Charname")
+        .map((cmd) => "`/" + cmd.command.name + "`")
+        .join(", ")}`,
+      ephemeral: true,
+    });
+  },
 }).toJSON();
